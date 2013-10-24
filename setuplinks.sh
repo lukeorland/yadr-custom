536ac7c9772a6e1dda7a687d9459ac96bc222da4
Luke Orland <luke@voxy.com>
2013-10-24 08:14:53 -0400

    script to quickly set up symlinks

---
+++ setuplinks.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+ln -sf $PWD/_gvimrc $HOME/.gvimrc
+ln -sf $PWD/_vimrc.after $HOME/.vimrc.after
+ln -sf $PWD/_vimrc.before $HOME/.vimrc.before
+ln -sf $PWD/_zsh.after $HOME/.zsh.after
+ln -sf $PWD/_zsh.before $HOME/.zsh.before
