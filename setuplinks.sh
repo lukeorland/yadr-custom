079071df122a5ad2d4338e07142e55d24d679175
Luke Orland <orluke@gmail.com>
2013-10-28 23:27:20 -0400

    remove zsh after before directories before creating symlinks

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,6 +1,9 @@
 #/bin/bash
-ln -sf $PWD/_gvimrc $HOME/.gvimrc
-ln -sf $PWD/_vimrc.after $HOME/.vimrc.after
-ln -sf $PWD/_vimrc.before $HOME/.vimrc.before
-ln -sf $PWD/_zsh.after $HOME/.zsh.after
-ln -sf $PWD/_zsh.before $HOME/.zsh.before
+# Run this from $HOME
+cd $HOME
+rmdir .zsh.{after,before}
+ln -sf $HOME/.yadr-custom/_gvimrc $HOME/.gvimrc
+ln -sf $HOME/.yadr-custom/_vimrc.after $HOME/.vimrc.after
+ln -sf $HOME/.yadr-custom/_vimrc.before $HOME/.vimrc.before
+ln -sf $HOME/.yadr-custom/_zsh.after $HOME/.zsh.after
+ln -sf $HOME/.yadr-custom/_zsh.before $HOME/.zsh.before
